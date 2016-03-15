bdd427768466efe263c9030d0a201f8c7e5cd46d
Ilya Vakarchuk <ilya.vakarchuk@noveogroup.com>
2016-03-15 09:42:09 +0600

    change gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -13,25 +13,31 @@ gulp.task('dependencies-install', function() {
 });
 
 gulp.task('bower-files', function() {
-  var jsFilter = gulpFilter('**/*.js', {restore : true}),
-    cssFilter = gulpFilter('**/*.css', {restore : true});
+
+  var jsFilter = gulpFilter('*.js');
+  var cssFilter = gulpFilter('*.css');
 
   return gulp.src(gulpBowerFiles())
+
+    // grab vendor js files from bower_components, minify and push in /public
     .pipe(jsFilter)
+    .pipe(gulp.dest('./assets/vendors' + '/js/'))
     .pipe(gulpUglify())
     .pipe(gulpRename({
-      suffix : ".min"
+      suffix: ".min"
     }))
-    .pipe(gulp.dest('./assets/vendors/js'))
-    .pipe(jsFilter.restore)
+    .pipe(gulp.dest('./assets/vendors' + '/js/'))
+    .pipe(jsFilter.restore())
 
+    // grab vendor css files from bower_components, minify and push in /public
     .pipe(cssFilter)
+    .pipe(gulp.dest('./assets/vendors' + '/css'))
     .pipe(gulpMinCss())
     .pipe(gulpRename({
-      suffix : ".min"
+      suffix: ".min"
     }))
-    .pipe(gulp.dest('./assets/vendors/css'))
-    .pipe(cssFilter.restore);
+    .pipe(gulp.dest('./assets/vendors' + '/css'))
+    .pipe(cssFilter.restore())
 });
 
 gulp.task('sass', function () {
